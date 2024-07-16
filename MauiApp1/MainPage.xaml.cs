f5caf223c871c0dad23494cac38a0b5aa0c5728a
Admin <Admin@HAMIT9>
2024-07-16 15:41:52 +0300

    Add project files.

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        int loc = 0;
+        String[] arrPics = { "animal1.jpeg", "animal2.jpeg", "animal3.jpeg"};
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        private void GoUpClicked(object sender, EventArgs e)
+        {
+            loc = (loc+1) % arrPics.Length;
+            ChangeImg.Source = arrPics[loc];
+
+        }
+
+        private void GoDownClicked(object sender, EventArgs e)
+        {
+            loc--;
+            if(loc < 0) loc = arrPics.Length - 1;
+
+            ChangeImg.Source = arrPics[loc];
+
+        }
+    }
+
+}
---
+++ MauiApp1/MauiProgram.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace MauiApp1
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Cinzel-VariableFont_wght.ttf", "CinzelVar");
+                    fonts.AddFont("Pacifico-Regular.ttf", "Pacifico");
+                    fonts.AddFont("RubikDoodleShadow-Regular.ttf", "RubikDoodle");
+                    fonts.AddFont("MaterialSymbolsOutlined-VariableFont-wght.ttf", "MaterialSymbolsOutlined");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
